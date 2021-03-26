88e9aa2edbd3f3fbfd03ecf4b1d64eb16cef0f75
Charlotte-91 <c.smallwood.smith@gmail.com>
2021-03-26 11:24:12 +0000

    Creating team comp form page

--- src/components/TeamCompForm.vue
+++ src/components/TeamCompForm.vue
@@ -1,18 +1,50 @@
 <template>
     <body>
-        <label class="typo__label">UID:</label>
-        <input type='number' v-model="this.UID" min='0' max='60' /> 
+      <div class ="form">
+        <div class="col-25">
+          <label class="typo__label">Player Name:</label>
+        </div>
+        <div class="col-75">
+          <div class="multiselect">
+            <input class="multiselect__tags" type='input' v-model="playerName" min='0' max='60' /> 
+          </div>
+        </div>
 
+        <div class="col-25">
+          <label class="typo__label">UID:</label>
+        </div>
+        <div class="col-75">
+          <div class="multiselect">
+            <input class="multiselect__tags" type='input' v-model="UID" min='0' max='60' /> 
+          </div>
+        </div>
+        <div class="col-25">
+          <label class="typo__label">Spiral Abyss Progress:</label>
+        </div>
+        <div class="col-75">
+          <div class="multiselect">
+            <input class="multiselect__tags" type='input' v-model="SA" min='0' max='60' /> 
+          </div>
+        </div>
+
+      <div class="col-25">
         <label class="typo__label">Card Design:</label>
+      </div>
+      <div class="col-75">
         <multiselect v-model="cardValue" :options="cardOptions" :searchable="false" :close-on-select="true" :show-labels="false" placeholder="Pick a value"></multiselect>
+      </div>
 
-
-    <label class="typo__label">Team: </label>
+      <div class="col-25">
+        <label class="typo__label">Team: </label>
+      </div>
+      <div class="col-75">
         <multiselect v-model="value" :options="options" :multiple="true" :max="4" :close-on-select="false" :clear-on-select="false" :preserve-search="true" placeholder="Pick your team" label="name" track-by="name" :preselect-first="false">
             <template slot="selection" slot-scope="{ values, isOpen }"><span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">{{ values.length }} options selected</span></template>
         </multiselect>
-        <button class='mobile-button' v-on:click=submit()>Submit</button>
-    </body>
+      </div>
+      <button class='mobile-button' v-on:click=submit()>Submit</button>
+    </div>
+  </body>
     
 
 </template>
@@ -71,6 +103,56 @@ export default {
 
 
 <style>
+@font-face { font-family: SuezOne-Regular; 
+  src: url('../assets/SuezOne-Regular.ttf'); } 
+body{
+  background-color:#464545;
+  font-family: SuezOne-Regular; 
+  font-size: 25px;
+}
+.col-25 {
+  float: left;
+  width: 40%;
+  text-align: right;
+  font-family: SuezOne-Regular; 
+  margin-bottom: 2px;
+}
+.col-75 {
+  float: right;
+  width: 60%;
+  margin-bottom: 2px;
+}
+.form {
+  position: relative;
+  top: 70%;
+  left: 50%;
+  transform: translate(-53%, 0%);
+  width: 45%;
+  background-color: #F9F6F2;
+  border: 36px solid orange;
+  border-radius: 10px;
+  margin: 30px;
+  padding: 10px;
+
+  border-image:
+      url("../assets/Form-bg.png")
+      70 / 50px    
+      round;                  
+}
+
+button {
+  background-color: #464545;
+  color: white;
+  padding: 10px 120px;
+  text-align: center;
+  font-size: 25px;
+  margin: auto;
+  cursor: pointer;
+  border-radius: 4px;
+  font-family: SuezOne-Regular; 
+
+}
+
 fieldset[disabled] .multiselect {
   pointer-events: none;
 }
