7c9ba846b84b22db1f06870b8eb7128d425d6fb0
Charlotte-91 <c.smallwood.smith@gmail.com>
2021-04-21 16:27:30 +0100

    Added a splash page

---
+++ src/components/SplashSelection.vue
@@ -0,0 +1,124 @@
+<template>
+  <div className="SplashSelection">
+        <div class="row">
+            <div class="column">
+                <router-link to="/">
+                    <div class="container">
+                        <img src="../assets/SplashPage/Basic.png"/>
+                        <div class="overlay">
+                            <div class="text">Basic card</div>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/team-form">
+                    <div class="container">
+                        <img src="../assets/SplashPage/Characomp.png"/>
+                        <div class="overlay">
+                            <div class="text">Character loadout card</div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+            <div class="column">
+                <router-link to="/">
+                    <div class="container">
+                        <img src="../assets/SplashPage/Multiplayer.png"/>
+                        <div class="overlay">
+                            <div class="text">Multiplayer card</div>
+                        </div>
+                    </div>
+                </router-link>
+                <router-link to="/team-form">
+                    <div class="container">
+                        <img src="../assets/SplashPage/Teamcomp.png"/>
+                        <div class="overlay">
+                            <div class="text">Team Composition card</div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+      </div>
+</template>
+
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+}
+
+/* Create four equal columns that sits next to each other */
+.column {
+  flex: 25%;
+  max-width: 50%;
+  padding: 0 15px;
+}
+
+.column img {
+  margin-top: 15px;
+  vertical-align: middle;
+  width: 100%;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 800px) {
+  .column {
+    flex: 50%;
+    max-width: 50%;
+  }
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 800px) {
+  .column {
+    flex: 100%;
+    max-width: 100%;
+  }
+}
+.container {
+  position: relative;
+  width: 100%;
+}
+
+/* .image {
+  display: block;
+  width: 100%;
+  height: auto;
+} */
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #464545;
+}
+
+.container:hover .overlay {
+  opacity: 0.8;
+}
+
+.text {
+  color: white;
+  font-size: 40px;
+  position: absolute;
+  font-family: SuezOne-Regular; 
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+title {
+    margin: 25px;
+    height: 1000px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -60,6 +60,15 @@ const routes = [
   // which is lazy-loaded when the route is visited.
   component: () => import(/* webpackChunkName: "CardDesign" */ '../views/TeamCompCard.vue'),
   props: true
+},
+{
+  path: '/splash-page',
+  name: 'Splash page',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "CardDesign" */ '../views/SplashPage.vue'),
+  props: true
 }
 
 ]
