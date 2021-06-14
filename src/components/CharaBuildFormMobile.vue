fedba502c9dade8d47c72f7000835748ac0a9b30
Charlotte-91 <c.smallwood.smith@gmail.com>
2021-06-14 18:14:24 +0100

    Changed character level to max 90

--- src/components/CharaBuildFormMobile.vue
+++ src/components/CharaBuildFormMobile.vue
@@ -31,7 +31,7 @@
           </div>
             <div class="col-75">
               <FormulateInput
-               type='number' name="level" validation="required|number|between:0,61" /> 
+               type='number' name="level" validation="required|number|between:0,91" /> 
             </div>
           </div>
 
